df401f364d818b035b8fe9f51f0374d945893f97
Amine2207 <mohamedamine.laribi@esprit.tn>
2021-02-26 22:31:27 +0100

    Second commit

--- src/App.js
+++ src/App.js
@@ -38,6 +38,12 @@ function App() {
   return (
     <>
       <Switch>
+
+
+        <Route exact path="/">
+          <Home />
+        </Route>
+
         <Route exact path="/">
           <Home />
         </Route>
